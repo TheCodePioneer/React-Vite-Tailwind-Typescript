7427c024e4e9f5511ecf54220e7f0ab2e6dd812a
TheCodePioneer <150111051+TheCodePioneer@users.noreply.github.com>
2025-10-02 15:26:30 +0400

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-green-100">
+      <h1 className="text-4xl font-bold text-green-700">
+        Hello React + Vite + Tailwind + TypeScript!
+      </h1>
+    </div>
+  )
+}
+
+export default App
